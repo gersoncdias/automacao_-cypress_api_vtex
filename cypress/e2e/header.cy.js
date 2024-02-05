d1f9d0d18fccc21320f2fda81869ba8fa39d1c29
Gerson Dias <gersoncdias1@gmail.com>
2024-02-04 23:49:03 -0300

    ajuste teste header

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,32 +1,48 @@
 const user = Cypress.env('user_name')
 const password = Cypress.env('user_password')
 
-describe('Deve validar o Header Vivara Lagado API', () => {
+describe('Deve validar o Header Lagado API', () => {
   beforeEach(() => {
-    cy.generateAndValidateToken(user, password).then((authToken) => {
-      cy.setCookie('VtexIdclientAutCookie_lojavivara', authToken)
-      cy.visit(Cypress.env('url'))
+    cy.session('authenticatedSession', () => {
+      cy.generateAndValidateToken(user, password).then((authToken) => {
+        cy.setCookie('VtexIdclientAutCookie_lojavivara', authToken)
+        cy.visit(Cypress.env('url'))
+      })
+    })
+  })
+  it('Em Vivara', () => {
+    cy.visit(Cypress.env('url'), {})
+    cy.header_vivara()
+    cy.topbar_vivara()
+  })
+  it('Em Quiz', () => {
+    cy.visit(`${Cypress.env('url')}${Cypress.env('quiz')}`)
+    cy.header_vivara()
+    cy.topbar_vivara()
+  })
+  it('Em Relogio', () => {
+    cy.visit(`${Cypress.env('url')}${Cypress.env('relogio')}`)
+    cy.header_vivara()
+    cy.topbar_vivara()
+  })
+})
+describe.only('Deve validar o Header Vivara não Lagado', () => {
+  beforeEach(() => {
+    Cypress.on('before:browser:launch', (browser, launchOptions) => {
+      if (browser.name === 'chrome') {
+        launchOptions.args.push('--use-fake-ui-for-media-stream')
+        launchOptions.args.push('--use-fake-device-for-media-stream')
+      }
+      return launchOptions
     })
     cy.visit(Cypress.env('url'), {})
   })
-  afterEach(() => {
-    cy.check_user()
-  })
   it('Em Vivara', () => {
     cy.header_vivara()
     cy.topbar_vivara()
   })
 })
-describe('Deve validar o Header Vivara não Lagado', () => {
-  beforeEach(() => {
-    cy.visit(Cypress.env('url'), {})
-  })
-  it('Em Vivara', () => {
-    cy.header_vivara()
-    cy.topbar_vivara()
-  })
-})
-describe('Deve validar o Header em outras paginas', () => {
+describe('Deve validar o Header em outras páginas Lagado API', () => {
   it('Em Quiz', () => {
     cy.visit(`${Cypress.env('url')}${Cypress.env('quiz')}`)
     cy.header_vivara()
